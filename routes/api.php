b8264f45172a2b4dd98460c33366b5e1dc9f02ad
Pamiralhp <132684622+Pamiralhp@users.noreply.github.com>
2023-11-05 20:28:23 +0100

    feat/User-Routes
    everything to make the project work adding routes for users and using middleware to protected it

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,17 @@ Route::post('login', [UserController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     
     Route::middleware('role:player')->group(function () {
+        Route::put('/players/{id}', [UserController::class, 'update']); // Update player name
         Route::post('/players/{id}/play', [GameController::class, 'throwDice']); // Player makes a move
         Route::delete('/players/{id}/delete', [GameController::class, 'destroy']); // Delete all moves for player X
         Route::get('/players/{id}/games', [GameController::class, 'listGames']); // List moves for player X
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('players', [UserController::class, 'listPlayers']); // Return all players
+        Route::get('/players/ranking', [UserController::class, 'ranking']); // Average success rate
+        Route::get('/players/ranking/loser', [UserController::class, 'loser']); // Player with the lowest success rate
+        Route::get('/players/ranking/winner', [UserController::class, 'winner']); // Player with the highest success rate
     });
 
     Route::post('logout', [UserController::class, 'logout']);
